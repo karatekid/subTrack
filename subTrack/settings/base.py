cade397e316994f835e78c2a307ad712853c691d
karatekid <mikechri@umich.edu>
2014-05-17 15:16:14 -0400

    got rid of uneccessary settings

--- subTrack/settings/base.py
+++ subTrack/settings/base.py
@@ -86,19 +86,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
-# here() gives us file paths from the root of the system to the
-# directory
-# holding the current file.
-here = lambda * x:
-os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
-
-PROJECT_ROOT = here("..")
-# root() gives us file paths from the root of the system to whatever
-# folder(s) we pass it starting at the parent directory of the current
-# file.
-root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
